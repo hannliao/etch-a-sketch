1ace1ed41933c608cf063d68b8ec2b3add289d09
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-01-30 16:29:08 -0700

    Add hover effect

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let gridSize = 16;
 for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("colored");
+    });
     grid.append(square);
 }
