387939f9834347bbbef5aa7d4faa3afa9b153332
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-01-30 20:20:15 -0700

    Add prompt to get number of squares per side

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-const grid = document.querySelector("#container");
-let gridSize = 16;
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("colored");
-    });
-    grid.append(square);
+function isValid() {
+    if (isNaN(userInput) || userInput > 100 || userInput <= 0) {
+        return false;
+    }
+    return true;
 }
+
+function createGrid() {
+    for (let i = 0; i < userInput * userInput; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareWidth}px`;
+        square.style.height = `${squareHeight}px`;
+    
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("colored");
+        });
+        grid.append(square);
+    }
+}
+
+function removeSquares() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+const grid = document.querySelector("#container");
+const btn = document.querySelector("#squaresPerSide");
+
+// create default 16x16 grid
+let userInput = 16;
+let squareWidth = 40;
+let squareHeight = 40;
+createGrid();
+
+btn.addEventListener("click", () => {
+    do {
+        userInput = prompt("enter number of squares per side (max 100): ");
+        if (isValid()) {
+            break;
+        }
+    } while (true);
+
+    // create new grid in the same total space as original grid (640px square)
+    squareWidth = 640 / userInput;
+    squareHeight = 640 / userInput;
+    removeSquares();
+    createGrid();
+});
